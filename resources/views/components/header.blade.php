0f2c3ca945f6dca99e272b7fc2ded64e2b7ddbf8
JohnDias0 <dias44520@gmail.com>
2024-03-07 22:42:12 -0300

    fix: header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -9,19 +9,27 @@
     <title>Document</title>
 </head>
 <body>
-<div class="bg-yellow-300 py-4 px-6 flex justify-between items-center">
+<div class="bg-yellow-300 py-4 px-6 mr-7 flex justify-between items-center">
     <!-- Logo -->
     <div class="flex items-center">
-        <img src="caminho_para_sua_logo.png" alt="Logo" class="h-8 w-auto mr-4">
-        <span class="text-lg font-bold">Minha Empresa</span>
-    </div>
+    <img src="caminho_para_sua_logo.png" alt="Logo" class="h-8 w-auto mr-2">
+</div>
+
 
     <!-- Opções de Página -->
-    <ul class="flex items-center space-x-6">
-        <li><a href="{{ route('home') }}" class="text-gray-800 hover:text-gray-600">Home</a></li>
-        <li><a href="{{ route('aboutus') }}" class="text-gray-800 hover:text-gray-600">Sobre Mim</a></li>
-        <li><a href="{{ route('contact') }}" class="text-gray-800 hover:text-gray-600">Contato</a></li>
+    <ul class="flex items-center justify-center flex-grow">
+        <li class="flex-grow">
+            <a href="{{ route('home') }}" class="text-gray-800 hover:text-gray-600">Home</a>
+        </li>
+        <li class="flex-grow">
+            <a href="{{ route('aboutus') }}" class="text-gray-800 hover:text-gray-600">Sobre Mim</a>
+        </li>
+        <li class="flex-grow">
+            <a href="{{ route('contact') }}" class="text-gray-800 hover:text-gray-600">Contato</a>
+        </li>
     </ul>
 </div>
+
+
 </body>
 </html>
